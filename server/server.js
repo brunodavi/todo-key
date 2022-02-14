5e5ec6255fa2087f67057d000bca975c145c71c6
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-02-14 19:59:30 -0300

    feat: pacotes json atualizados & cors configurado

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
 
-app.use(cors());
+const CORS = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE'],
+};
+
+app.use(cors(CORS));
 
 app.listen(PORT, () => {
   console.log(`Rodando api em http://localhost:${PORT}`);
